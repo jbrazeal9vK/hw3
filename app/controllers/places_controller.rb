2202dea2874ad62226650ecb94811bfdcd928a2e
Jim Brazeal <jim.brazeal@kellogg.northwestern.edu>
2023-05-16 14:30:57 +0000

    Second commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,10 @@
 class PlacesController < ApplicationController
   
   def index
-    
-    
+    place = params["new"]
 
+    @places = ["Mexico City","Charleston","Beijing","Amsterdam"]
+    
   end
 
 end
